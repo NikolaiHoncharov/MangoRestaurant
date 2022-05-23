f39f58904980ee7c48a20343a157118497739798
Гончаров Николай <goncharov.nikolia@gmail.com>
2022-05-23 19:31:43 +0300

    add 2 API profect

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -1,6 +1,7 @@
 using Mango.Web;
 using Mango.Web.Services;
 using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,29 @@ SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "Cookies";
+    options.DefaultChallengeScheme = "oidc";
+})
+                .AddCookie("Cookies", c => c.ExpireTimeSpan = TimeSpan.FromMinutes(10))
+                .AddOpenIdConnect("oidc", options =>
+                {
+                    options.Authority = builder.Configuration["ServiceUrls:IdentityAPI"];
+                    options.GetClaimsFromUserInfoEndpoint = true;
+                    options.ClientId = "mango";
+                    options.ClientSecret = "secret";
+                    options.ResponseType = "code";
+                    options.ClaimActions.MapJsonKey("role", "role", "role");
+                    options.ClaimActions.MapJsonKey("sub", "sub", "sub");
+                    options.TokenValidationParameters.NameClaimType = "name";
+                    options.TokenValidationParameters.RoleClaimType = "role";
+                    options.Scope.Add("mango");
+                    options.SaveTokens = true;
+
+                });
+
 
 var app = builder.Build();
 
@@ -25,7 +49,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
--- Mango.Web/Services/IServices/IProductService.cs
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -4,10 +4,10 @@ namespace Mango.Web.Services.IServices
 {
     public interface IProductService : IBaseService
     {
-        Task<T> GetAllProductsAsync<T>(/*string token*/);
-        Task<T> GetProductByIdAsync<T>(int id/*, string token*/);
-        Task<T> CreateProductAsync<T>(ProductDto productDto/*, string token*/);
-        Task<T> UpdateProductAsync<T>(ProductDto productDto/*, string token*/);
-        Task<T> DeleteProductAsync<T>(int id/*, string token*/);
+        Task<T> GetAllProductsAsync<T>(string token);
+        Task<T> GetProductByIdAsync<T>(int id, string token);
+        Task<T> CreateProductAsync<T>(ProductDto productDto, string token);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto, string token);
+        Task<T> DeleteProductAsync<T>(int id, string token);
     }
 }
