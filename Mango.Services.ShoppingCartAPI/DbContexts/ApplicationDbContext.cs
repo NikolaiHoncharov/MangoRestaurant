f8462ce6ff3abfd6924c02f7db60536c7cb092f7
Гончаров Николай <goncharov.nikolia@gmail.com>
2022-05-23 22:03:59 +0300

    add migrations and dataset model

--- Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
+++ Mango.Services.ShoppingCartAPI/DbContexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.DbContexts
 {
@@ -8,7 +9,9 @@ namespace Mango.Services.ShoppingCartAPI.DbContexts
             : base(options)
         {
         }
-        //public DbSet<Product> Products { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
 
     }
 }
